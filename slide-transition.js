81d9ba1c12a4efa082deb88a2237f94b094fed5f
thvinicc <thbrazini@gmail.com>
2022-06-05 16:57:02 -0300

    Create slide-transition.js

---
+++ slide-transition.js
@@ -0,0 +1,13 @@
+new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  initialSlide: 2,
+  speed: 1000,
+  spaceBetween: 32,
+  loop: true,
+  centeredSlides: true,
+  roundLengths: true,
+  mousewheel: true,
+  grabCursor: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true } });

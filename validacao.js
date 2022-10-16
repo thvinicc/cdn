fd58f053d9e222d34dab0b80584f43cef01c568d
thvinicc <thbrazini@gmail.com>
2022-10-15 22:00:50 -0300

    Create validacao.js

---
+++ validacao.js
@@ -0,0 +1,11 @@
+const sessions = ['cytotec', 'bunda', 'abortivo']
+const searchTarget = document.querySelector('.target');
+const submitButton = document.querySelector('.btn btn-primary rtcl-submit-btn yp_onscreen')
+
+if (searchTarget.textContent.includes('session')) {
+  if (!sessions.every((session) => searchTarget.textContent.includes(session))) {
+    submitButton.setAttribute("disabled", "")
+  } else {
+    submitButton.removeAttribute('disabled')
+  }
+}

be16811c29bebd3fc598bfa1b7950bb908c72d24
thvinicc <thbrazini@gmail.com>
2022-10-15 22:05:15 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -1,5 +1,5 @@
 const sessions = ['cytotec', 'bunda', 'abortivo']
-const searchTarget = document.querySelector('.target');
+const searchTarget = document.querySelector('.mce-content-body description post-type-page post-status-publish page-template-default locale-pt-br mceContentBody webkit wp-editor html5-captions');
 const submitButton = document.querySelector('.btn btn-primary rtcl-submit-btn yp_onscreen')
 
 if (searchTarget.textContent.includes('session')) {

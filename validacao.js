7116fb2586f0bbe65b0146d73daa55829f55ba38
thvinicc <thbrazini@gmail.com>
2022-10-23 11:05:00 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -1,3 +1,3 @@
 Array.from( document.querySelectorAll('div.rtcl-listing-description') )
-  .filter( node => /\b(Cytotec|Guarapari)\b/i.test(node.textContent) )
+  .filter( node => /\b(Cytotec|cytotec)\b/i.test(node.textContent) )
   .forEach( node => node.style.display = 'none' );

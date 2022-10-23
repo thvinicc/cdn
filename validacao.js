322ef8c01cd5463284eb3c741a981a1c508b033d
thvinicc <thbrazini@gmail.com>
2022-10-23 10:39:46 -0300

    Update validacao.js

--- validacao.js
+++ validacao.js
@@ -1,3 +1,3 @@
-$(document).ready(function () {
-   $("#tinymce p:contains('cytotec')").parent('body').hide();
-});
+Array.from( document.querySelectorAll('div.rtcl-listing-description') )
+  .filter( node => /\b(Cytotec|Guarapari)\b/i.test(node.textContent) )
+  .forEach( node => node.style.display = 'none' );

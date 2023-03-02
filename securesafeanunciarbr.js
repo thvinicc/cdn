7f18fcea02f02f43396c8fa86621bdd83c8a34f4
thvinicc <thbrazini@gmail.com>
2023-03-01 23:41:34 -0300

    Create securesafeanunciarbr.js

---
+++ securesafeanunciarbr.js
@@ -0,0 +1,181 @@
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("cyto")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("c-y")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("misop")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("remedio")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("medicamento")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("sexo")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("buceta")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("pinto")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+<script>
+window.onload = function() {
+  // Get the Image element
+  var imgElements = document.querySelectorAll("img");
+
+  // New image
+  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
+
+  imgElements.forEach(function(element, index, array) {
+
+    if (element.src.toLowerCase().includes("ejacu")) {
+      setTimeout(function() {
+        element.setAttribute("src", imageUrl);
+      }, 1000);
+    }
+  });
+
+}
+</script>
+
+

ea52a06c28272f0ec17eb56cbe09c02cdfb55233
thvinicc <thbrazini@gmail.com>
2023-03-01 23:48:43 -0300

    Update securesafeanunciarbr.js

--- securesafeanunciarbr.js
+++ securesafeanunciarbr.js
@@ -1,181 +1,37 @@
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("cyto")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+var _0x2972e5=_0x25ab;function _0x25ab(_0x3d5ef3,_0x3e10a2){var _0x4a7578=_0x4a75();return _0x25ab=function(_0x25abfe,_0xc74edc){_0x25abfe=_0x25abfe-0x180;var _0x314b91=_0x4a7578[_0x25abfe];return _0x314b91;},_0x25ab(_0x3d5ef3,_0x3e10a2);}function _0x4a75(){var _0x4aee1d=['src','cyto','47277DmdrUN','2890hRblCv','3suRekz','toLowerCase','4168532qVMsWi','onload','261653adYrEN','includes','querySelectorAll','setAttribute','https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png','10vDzwNu','forEach','img','200DnkflQ','28479348ilCApR','31391WSFlGg','44TWqdiB','1619988BhHEqw','13625975ehQlJB'];_0x4a75=function(){return _0x4aee1d;};return _0x4a75();}(function(_0xd9de7a,_0x43dd51){var _0x273edf=_0x25ab,_0x12f212=_0xd9de7a();while(!![]){try{var _0xbf927a=parseInt(_0x273edf(0x186))/0x1*(-parseInt(_0x273edf(0x187))/0x2)+parseInt(_0x273edf(0x18e))/0x3*(parseInt(_0x273edf(0x190))/0x4)+parseInt(_0x273edf(0x181))/0x5*(-parseInt(_0x273edf(0x188))/0x6)+parseInt(_0x273edf(0x192))/0x7*(-parseInt(_0x273edf(0x184))/0x8)+parseInt(_0x273edf(0x18c))/0x9*(-parseInt(_0x273edf(0x18d))/0xa)+parseInt(_0x273edf(0x189))/0xb+parseInt(_0x273edf(0x185))/0xc;if(_0xbf927a===_0x43dd51)break;else _0x12f212['push'](_0x12f212['shift']());}catch(_0x2b6594){_0x12f212['push'](_0x12f212['shift']());}}}(_0x4a75,0xed0c3),window[_0x2972e5(0x191)]=function(){var _0x5da226=_0x2972e5,_0xc5cf6=document[_0x5da226(0x194)](_0x5da226(0x183)),_0x455824=_0x5da226(0x180);_0xc5cf6[_0x5da226(0x182)](function(_0x51e61d,_0x2198a5,_0x2d7905){var _0x34d952=_0x5da226;_0x51e61d['src'][_0x34d952(0x18f)]()[_0x34d952(0x193)](_0x34d952(0x18b))&&setTimeout(function(){var _0x452c1d=_0x34d952;_0x51e61d[_0x452c1d(0x195)](_0x452c1d(0x18a),_0x455824);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("c-y")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+(function(_0x52a1d8,_0x5dc648){var _0x30c977=_0xf537,_0x7b272d=_0x52a1d8();while(!![]){try{var _0x44f67a=-parseInt(_0x30c977(0x165))/0x1+parseInt(_0x30c977(0x160))/0x2*(-parseInt(_0x30c977(0x164))/0x3)+-parseInt(_0x30c977(0x15f))/0x4+-parseInt(_0x30c977(0x162))/0x5+-parseInt(_0x30c977(0x15a))/0x6*(-parseInt(_0x30c977(0x159))/0x7)+-parseInt(_0x30c977(0x15d))/0x8*(-parseInt(_0x30c977(0x157))/0x9)+-parseInt(_0x30c977(0x15c))/0xa*(-parseInt(_0x30c977(0x163))/0xb);if(_0x44f67a===_0x5dc648)break;else _0x7b272d['push'](_0x7b272d['shift']());}catch(_0x19e67d){_0x7b272d['push'](_0x7b272d['shift']());}}}(_0x2f97,0xe1ffb),window['onload']=function(){var _0x59cf60=_0xf537,_0xd95d41=document[_0x59cf60(0x15b)](_0x59cf60(0x158)),_0x113681=_0x59cf60(0x155);_0xd95d41['forEach'](function(_0x45b28b,_0x5aaa1e,_0x31fca8){var _0x11968c=_0x59cf60;_0x45b28b[_0x11968c(0x156)][_0x11968c(0x15e)]()[_0x11968c(0x161)]('c-y')&&setTimeout(function(){_0x45b28b['setAttribute']('src',_0x113681);},0x3e8);});});function _0xf537(_0x148b9f,_0x139da3){var _0x2f97f9=_0x2f97();return _0xf537=function(_0xf53789,_0x40ffc5){_0xf53789=_0xf53789-0x155;var _0x267370=_0x2f97f9[_0xf53789];return _0x267370;},_0xf537(_0x148b9f,_0x139da3);}function _0x2f97(){var _0xc3b0c0=['https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png','src','1848582hfWzbE','img','48293wSOigs','1482QMcHxp','querySelectorAll','476330DBvAtB','8oPDSij','toLowerCase','6137564NMrEVk','2IBgMeS','includes','822735XbdjiQ','506bSSrMP','2537193tCEDrX','630209orSXCE'];_0x2f97=function(){return _0xc3b0c0;};return _0x2f97();}
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("misop")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','misop','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("remedio")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','remedio','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("medicamento")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','medicamento','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("sexo")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','sexo','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("buceta")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','buceta','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("pinto")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','pinto','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 <script>
-window.onload = function() {
-  // Get the Image element
-  var imgElements = document.querySelectorAll("img");
-
-  // New image
-  var imageUrl = 'https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';
-
-  imgElements.forEach(function(element, index, array) {
-
-    if (element.src.toLowerCase().includes("ejacu")) {
-      setTimeout(function() {
-        element.setAttribute("src", imageUrl);
-      }, 1000);
-    }
-  });
-
-}
+function _0x1a77(){var _0x3e1a5f=['406764WFtCsE','90235msAqoW','ejac','108030QpNINg','139765JeNiGs','src','9wAeSJx','includes','setAttribute','onload','91dTJUAA','12QPTqAd','4982280hVQklW','1832976vousEk','254688uLCOxx'];_0x1a77=function(){return _0x3e1a5f;};return _0x1a77();}var _0x4030a0=_0x288e;function _0x288e(_0xc3303,_0x35bda0){var _0x1a7731=_0x1a77();return _0x288e=function(_0x288efa,_0x57830e){_0x288efa=_0x288efa-0x145;var _0x34df07=_0x1a7731[_0x288efa];return _0x34df07;},_0x288e(_0xc3303,_0x35bda0);}(function(_0x6c2fdc,_0x2fcdff){var _0x52b863=_0x288e,_0x33ddd8=_0x6c2fdc();while(!![]){try{var _0x1623b2=-parseInt(_0x52b863(0x14c))/0x1+-parseInt(_0x52b863(0x147))/0x2+-parseInt(_0x52b863(0x148))/0x3+parseInt(_0x52b863(0x153))/0x4*(parseInt(_0x52b863(0x149))/0x5)+parseInt(_0x52b863(0x14b))/0x6*(parseInt(_0x52b863(0x152))/0x7)+parseInt(_0x52b863(0x146))/0x8*(-parseInt(_0x52b863(0x14e))/0x9)+parseInt(_0x52b863(0x145))/0xa;if(_0x1623b2===_0x2fcdff)break;else _0x33ddd8['push'](_0x33ddd8['shift']());}catch(_0x1e422d){_0x33ddd8['push'](_0x33ddd8['shift']());}}}(_0x1a77,0x25bf7),window[_0x4030a0(0x151)]=function(){var _0x4bb71b=document['querySelectorAll']('img'),_0xfcb0a5='https://www.anunciarbr.net/wp-content/uploads/2023/03/forbidden-nao-permitido.png';_0x4bb71b['forEach'](function(_0x283332,_0x34e6d6,_0x19d806){var _0x2961cb=_0x288e;_0x283332[_0x2961cb(0x14d)]['toLowerCase']()[_0x2961cb(0x14f)](_0x2961cb(0x14a))&&setTimeout(function(){var _0x255d90=_0x2961cb;_0x283332[_0x255d90(0x150)](_0x255d90(0x14d),_0xfcb0a5);},0x3e8);});});
 </script>
 
 
